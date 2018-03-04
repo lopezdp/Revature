d8be1bf17ea7e96b96e48b13d456efcfdc1ab6f0
LopezDP <davidplopez@live.com>
2018-03-04 01:51:06 -0500

    Completed Revature Statements in Java.

---
+++ Statements/src/examples/WhileLoop.java
@@ -0,0 +1,13 @@
+package examples;
+
+public class WhileLoop {
+	public static void main(String[] args) {
+		int count = 1;
+		while(count < 11) {
+			System.out.println(count);
+			count++;
+		}
+		System.out.println("We have exited the loop!");
+	}
+
+}
