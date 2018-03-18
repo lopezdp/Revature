b17b01f71660d7f38865c5d8b7e14d7c90d2d67e
LopezDP <davidplopez@live.com>
2018-03-17 23:23:02 -0400

    Inheritance samples complete.

---
+++ Inheritance/src/Overriding/MethodOverriding.java
@@ -0,0 +1,37 @@
+package Overriding;
+
+public class MethodOverriding {
+	
+	public static void makeItTalk(Animal input) {
+		input.speak();
+	}
+	
+	public static void main(String[] args) {
+		
+		Animal myAnimal = new Animal();
+		Dog myDog = new Dog();
+		
+		System.out.println("Make the Animal Object Speak: ");
+		makeItTalk(myAnimal);
+		
+		System.out.println("Make the Dog Object Speak: ");
+		makeItTalk(myDog);
+	
+	}
+
+}
+
+class Animal {
+	// Speak functionality
+	public void speak() {
+		System.out.println("Generic Animal Noise! \n");
+	}
+}
+
+class Dog extends Animal {
+	// Method Overriding for speak method!!
+	// Speak functionality
+	public void speak() {
+		System.out.println("Woof! Woof! \n");
+	}
+}
