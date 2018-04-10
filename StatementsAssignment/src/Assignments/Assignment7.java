4cd8907dbc61ffd739a5521eb423b3c69558efc3
LopezDP <davidplopez@live.com>
2018-04-09 22:06:02 -0400

    Completed Statements Assignment 7.

---
+++ StatementsAssignment/src/Assignments/Assignment7.java
@@ -0,0 +1,14 @@
+package Assignments;
+
+import java.util.Scanner;
+
+public class Assignment7 {
+	public static void main(String[] args) {
+		
+		for(int i = 0; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println("Output: " + i);
+			}	
+		}
+	}
+}
