613e97d932a1d503a7f64a7849356707c8ad5d8e
LopezDP <davidplopez@live.com>
2018-04-09 22:13:43 -0400

    Completed Statements Assignment 8.

---
+++ StatementsAssignment/src/Assignments/Assignment8.java
@@ -0,0 +1,17 @@
+package Assignments;
+
+public class Assignment8 {
+	public static void main(String[] args) {
+		int i = 0;
+		
+		do {
+			if (i % 2 != 0) {
+				System.out.println("Output: " + i);
+			}
+			
+			i++;
+			
+		} while(i < 10);
+	}
+
+}
