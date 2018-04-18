c04025f3a69e2c4ef9d3d825869fa0071f3a6f68
LopezDP <davidplopez@live.com>
2018-04-17 21:37:48 -0400

    Completed Strings Assignment 1.

---
+++ StringsAssignment/src/Assignments/Assignment1.java
@@ -0,0 +1,14 @@
+package Assignments;
+
+public class Assignment1 {
+	
+	public static void main(String[] args) {
+		
+		String name = "Lopez";
+		
+		String greeting = "Hello " + name;
+		
+		System.out.println(greeting);
+	}
+
+}
