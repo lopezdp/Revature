1e75084829166defc6cd521ffc3d4f4bcc0874ff
LopezDP <davidplopez@live.com>
2018-04-18 21:43:36 -0400

    Completed Strings Assignment 4.

---
+++ StringsAssignment/src/Assignments/Assignment4.java
@@ -0,0 +1,21 @@
+package Assignments;
+
+public class Assignment4 {
+	
+	/*
+	 * 
+	 * Write a class named Assignment4 that declares 
+	 * a string "abcdefghijklmnopqrstuvwxyz". Use the indexOf() 
+	 * method to print the index of "s" and "f".
+	 * 
+	 * 
+	 */
+	
+	public static void main(String[] args) {
+		
+		String s1 = "abcdefghijklmnopqrstuvwxyz";
+		System.out.println(s1.indexOf("s") + " & " + s1.indexOf("f"));
+		
+	}
+
+}
