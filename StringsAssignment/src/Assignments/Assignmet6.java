8da0ec380cee3ecf288a8a053dea168e063efbb8
LopezDP <davidplopez@live.com>
2018-04-21 11:05:29 -0400

    Completed Strings Assignment 6.

--- StringsAssignment/src/Assignments/Assignmet6.java
+++ StringsAssignment/src/Assignments/Assignmet6.java
@@ -16,11 +16,12 @@ public class Assignmet6 {
 	
 	public static void main(String[] args) {
 		String s1 = "Hello My Name is Java";
+		char c = ' ';
 		
 		int counter = 0;
 		
-		for(int i = 0; i <= s1.length(); i++) {
-			if(s1.charAt(i) == " ") {
+		for(int i = 0; i < s1.length(); i++) {
+			if(s1.charAt(i) == c) {
 				counter++;
 				
 				if(counter == 2) {
