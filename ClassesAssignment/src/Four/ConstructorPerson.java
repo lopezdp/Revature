ea3cb4495e9f3ccd9d258d29dcd3e31e20984323
LopezDP <davidplopez@live.com>
2018-04-29 22:20:22 -0400

    Completed ConstructorPerson Class Assignment Four.

---
+++ ClassesAssignment/src/Four/ConstructorPerson.java
@@ -0,0 +1,28 @@
+package Four;
+
+public class ConstructorPerson {
+	
+	/*
+	 * 
+	 * Write a class named ConstructorPerson 
+	 * that declares name (String) and age (int) 
+	 * instance variables. 
+	 * 
+	 * Write a no-arg constructor1 that initializes 
+	 * ConstructorPerson with default values. 
+	 * 
+	 * In a Main class, create an instance of a 
+	 * ConstructorPerson and print its properties name and age.  
+	 * 
+	 * Place these files in a package named, “four”. 
+	 * 
+	 */
+	String name;
+	int age;
+	
+	ConstructorPerson() {
+		this.name = "Dave";
+		this.age = 19;
+	}
+
+}
---
+++ ClassesAssignment/src/Four/MainClass.java
@@ -0,0 +1,11 @@
+package Four;
+
+public class MainClass {
+	
+	public static void main(String[] args) {
+		ConstructorPerson cp = new ConstructorPerson();
+		
+		System.out.println("This is the name: " + cp.name + " and this is the age: " + cp.age);
+	}
+
+}
